dbec9fe3d3853fe9086b12fc5149aa1e53fcb58e
adamehlers <adamehlers15@gmail.com>
2023-05-27 09:36:00 -0400

    Added error checking and ability to exit code when desired by the user

--- triangle_area.cpp
+++ triangle_area.cpp
@@ -18,6 +18,7 @@ double triangle_calculation(double side_a, double side_b, double side_c){
     if (side_a + side_b > side_c){
         double s = (side_a + side_b + side_c)/2;
         double area = sqrt(s * (s - side_a) * (s - side_b) * (s - side_c));
+        cout << "Area: " << area << " Units" << endl;
         return area;
     }
     else{
@@ -29,9 +30,34 @@ double triangle_calculation(double side_a, double side_b, double side_c){
 
 
 int main() {
-    while (quit_code_flag == false){
+    double first_side;
+    double second_side;
+    double third_side;
+    ///while (quit_code_flag == false){
+    do{
+        cout << "Enter first side value (Enter Q to quit): ";
+        cin >> first_side;
         
+        if (cin.fail()) {
+            cin.clear();
+            string character;
+            cin >> character;
+            if (character == "Q" || character == "q"){
+                cout << "Exiting code...";
+                quit_code_flag = true;
+                break;
+            }
+        }
+
+        cout << "Enter second side value (Enter Q to quit): ";
+        cin >> second_side;
+        cout << "Enter third side value (Enter Q to quit): ";
+        cin >> third_side;
+
+        triangle_calculation(first_side, second_side, third_side);
+
     }
+    while (quit_code_flag == false);
 
     return 0;
 }
