9a2ac1cc2c98d321c466810bb74db1ab3da3ef96
adamehlers <adamehlers15@gmail.com>
2023-05-27 09:15:33 -0400

    Added function to handle calculations

--- triangle_area.cpp
+++ triangle_area.cpp
@@ -11,8 +11,22 @@
 
 using namespace std;
 
-
 bool quit_code_flag = false;
+
+
+double triangle_calculation(double side_a, double side_b, double side_c){
+    if (side_a + side_b > side_c){
+        double s = (side_a + side_b + side_c)/2;
+        double area = sqrt(s * (s - side_a) * (s - side_b) * (s - side_c));
+        return area;
+    }
+    else{
+        cout << "Error: THe sum of the first two sides is not larger than the third side" << endl;
+        return 1; ///Break statement instead??
+    }
+
+}
+
 
 int main() {
     while (quit_code_flag == false){
