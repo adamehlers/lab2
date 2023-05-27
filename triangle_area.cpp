9b39d2cfa1022bac695a5379cae2dd7573fca50b
adamehlers <adamehlers15@gmail.com>
2023-05-27 09:01:48 -0400

    Added quit code flag and main function

--- triangle_area.cpp
+++ triangle_area.cpp
@@ -4,3 +4,20 @@
  *    @date: 05/27/2023
  *   @brief: This program will calculate the area of a triangle when three inputs are provided for the lengths of all the sides. If the sum of any two sides is greater than the third, the triangle is valid. The program will keep running until the user decides to quit.
 */
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+
+bool quit_code_flag = false;
+
+int main() {
+    while (quit_code_flag == false){
+        
+    }
+
+    return 0;
+}
