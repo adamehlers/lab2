b1a8ce52b31e6fc34d520f2350fe7cf2af0d3a40
adamehlers <adamehlers15@gmail.com>
2023-05-27 08:37:51 -0400

    Initial commit

---
+++ triangle_area.cpp
@@ -0,0 +1,6 @@
+/**
+ *    @file: triangle_area.cpp
+ *  @author: Adam Ehlers
+ *    @date: 05/27/2023
+ *   @brief: This program will calculate the area of a triangle when three inputs are provided for the lengths of all the sides. If the sum of any two sides is greater than the third, the triangle is valid. The program will keep running until the user decides to quit.
+*/
